cbce133951f66db3a1d5d8bd9ceedf6646a6c85f
Asmit Dhakal <107795383+Asmit-Dhakal@users.noreply.github.com>
2023-10-28 20:31:04 +0545

    first commit

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+let name='Ak47';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with me
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+         Deserunt temporibus suscipit voluptatum saepe fuga!
+          Repellat veritatis similique voluptate asperiores
+           iste quasi sit hic molestias, aperiam accusamus 
+           odit ipsam porro architecto mollitia placeat
+            temporibus minima odio!
+      </p>
     </div>
+    </>
+ 
   );
-}
+} 
 
 export default App;
