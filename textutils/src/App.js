be6cbe2cdcad6913e47e91ff624cc17bfc2616c3
Asmit Dhakal <107795383+Asmit-Dhakal@users.noreply.github.com>
2023-10-30 18:57:54 +0545

    first commit

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -4,32 +4,21 @@ function App() {
     <>
   <nav className="navbar navbar-expand-lg bg-body-tertiary"> 
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Navbar</a>
+    <a className="navbar-brand" href="/">Home</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <a className="nav-link active" aria-current="page" href="/">PCB Visualizer</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
+          <a className="nav-link" href="/">PCBAVisualizer</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+          <a className="nav-link" href="/">Summary</a>
+        </li>          
       </ul>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
@@ -37,6 +26,24 @@ function App() {
       </form>
     </div>
   </div>
+</nav>
+<div>
+
+</div>
+<nav className="navbar navbar-expand-lg bg-body-tertiary"> 
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">PCB Viewer</a>
+    <a className="navbar-brand" href="/">PCB Checker</a>
+    <a className="navbar-brand" href="/">Panel Editor</a>
+    <a className="navbar-brand" href="/">Buildup Editor</a>
+    <a className="navbar-brand" href="/">Layer Editor</a>
+    <a className="navbar-brand" href="/">Drill Editor</a>
+    <a className="navbar-brand" href="/">Outline/Milling Editor</a>
+    <a className="navbar-brand" href="/">Paste Editor</a>
+    <a className="navbar-brand" href="/">Marking Editor</a>
+    <a className="navbar-brand" href="/">Impedance Cacluator</a>
+    <a className="navbar-brand" href="/">Pixture Editor</a>
+  </div> 
 </nav>
 </>
   );
