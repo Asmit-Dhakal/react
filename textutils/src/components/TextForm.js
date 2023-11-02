167aa4a9f2b10364d46ff3cbbafb71c496cbf71f
Asmit Dhakal <107795383+Asmit-Dhakal@users.noreply.github.com>
2023-11-02 09:55:49 +0545

    first commit

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,22 +1,24 @@
 import React, {useState}from 'react'
 
 export default function TextForm(props) {
-  const [text,setText]=useState('Enter the text here');
-  console.log(useState('Enter the text here 2'));
+  
+  const handleUpClick=()=>{
+    console.log("Uppercase was clicked");
+  }
+
+  const handleOnChange=()=>{
+    console.log("On Change")
+  }
+  const [text,setText]=useState('Enter the text here 2');
   return (
     <div>
     <div className='container'>
-        <h1>{props.heading}- {text}</h1>
+      <h1>{props.heading}</h1>
 <div className="mb-3">
-  <textarea className="form-control" id="myBox" rows="8"></textarea>
+  <textarea className="form-control" value={text} onChange ={handleOnChange} id="myBox" rows="8"></textarea>
 </div> 
 </div>
-<button className="btn btn-primary">Uppercase</button>
-<div>
-<label for="myfile">Browse</label>
-<input type="file" id="myfile" name="myfile"></input>
-</div>
-
+<button className="btn btn-primary" onClick={handleUpClick}>Uppercase</button>
 </div>
   )
 }
